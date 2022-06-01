f2f1a00df693e0959ab0d00e17efe77c6f5dd303
Anton Smirnov <sandfox@sandfox.me>
2022-06-02 02:00:26 +0300

    Furo theme in docs

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+project = 'Pseudolocale'
+author = 'Anton Smirnov'
+copyright = '{} {}'.format(datetime.now().year, author)
+language = 'en'
+
+html_title = project
+html_theme = 'furo'
+templates_path = ["_templates"]
