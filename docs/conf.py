a58e0580cfa840869383b9dc0185a64232db6b8e
Anton Smirnov <sandfox@sandfox.me>
2023-06-28 20:04:16 +0300

    Change theme to book

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,17 @@
 from datetime import datetime
 
-project = 'Pseudolocale'
+# import specific project config
+import os, sys
+sys.path.append(os.curdir)
+from conf_project import *
+
 author = 'Anton Smirnov'
 copyright = '{} {}'.format(datetime.now().year, author)
 language = 'en'
+html_sidebars = {
+    "**": ["navbar-logo.html", "rtd-version.html", "icon-links.html", "sbt-sidebar-nav.html", "sidebar-ethical-ads.html"]
+}
 
 html_title = project
-html_theme = 'furo'
+html_theme = 'sphinx_book_theme'
 templates_path = ["_templates"]
